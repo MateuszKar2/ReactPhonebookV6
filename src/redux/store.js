05d0407f0d37f61cae9f40b59c069ce7775bdffa
MateuszKar2 <mateuszwasabi@gmial.com>
2024-09-05 23:11:15 +0200

    add Redux/ store

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+import contactReducer from "./contactSlice";
+import filterReducer from "./filterSlice";
+
+
+const store = configureStore({
+    reducer: {
+        contacts: contactReducer,
+        filter: filterReducer
+    }
+});
+
+export default store;
