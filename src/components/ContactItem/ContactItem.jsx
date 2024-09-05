344e9526105e33ace585d2075b612a1fb767c96c
MateuszKar2 <mateuszwasabi@gmial.com>
2024-09-05 23:30:06 +0200

    delete mistake

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PropTypes, { number } from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './ContactItem.module.css';
 
 const ContactItem = ({ id, name, number, onDeleteContact}) => {
