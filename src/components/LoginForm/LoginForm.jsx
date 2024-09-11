e7ef374fd001671c1e4fff888d36c88c7b955696
MateuszKar2 <mateuszwasabi@gmial.com>
2024-09-11 11:44:53 +0200

    add file

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import InputsForm from "components/InputsForm/InputsForm";
+
+const LoginForm = () => {
+
+    return(
+        <InputsForm buttonText="Log In" formType="login"/>
+    );
+};
+
+export default LoginForm;
