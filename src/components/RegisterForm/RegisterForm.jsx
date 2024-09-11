1ce1807262510e6951f2425fc5b1379c2519812c
MateuszKar2 <mateuszwasabi@gmial.com>
2024-09-11 11:45:04 +0200

    add file

---
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import InputsForm from "components/InputsForm/InputsForm";
+
+
+const RegisterForm = () => {
+    return(
+        <InputsForm buttonText="Register" formType="register"/>
+    )
+}
+
+export default RegisterForm
